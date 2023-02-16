837899c982dce468f115a3349809f1c22e0a8f0b
Ivan Selukov <blawiken@ya.ru>
2023-02-16 13:30:39 +0500

    auto_now_add

--- measurement/models.py
+++ measurement/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from datetime import date
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
 
@@ -11,4 +10,4 @@ class Sensor(models.Model):
 class Measurement(models.Model):
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
     temperature = models.DecimalField(decimal_places=1, max_digits=4)
-    created_at = models.DateField(default=date.today())
+    created_at = models.DateField(auto_now_add=True)
